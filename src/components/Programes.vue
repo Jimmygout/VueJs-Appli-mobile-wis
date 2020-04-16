c671634bdeba91f9d1056177f45555f32f913f4f
jeremdh62 <jerem.dha@gmail.com>
2020-04-16 16:16:48 +0200

    Vue programmes fini

--- src/components/Programes.vue
+++ src/components/Programes.vue
@@ -1,28 +1,8 @@
 <template>
          
-     
-         <!-- <v-timeline>
-
-            <v-timeline-item v-for="(item, i) in info" :key="i">
-              
-              <h2 slot="opposite">{{ formatDate(item.dateConcert) }}</h2>
-
-               <v-card  color="rgb(56, 95, 115)" dark class="elevation-3">
-                <div class="d-flex flex-no-wrap justify-space-between">
-                 <div>
-                    <v-card-title class="headline" v-text="item.chanteur"></v-card-title>
-                    <v-card-subtitle v-text="item.designation"></v-card-subtitle>
-                 </div>
-
-              <v-avatar class="ma-0" size="120" tile>
-                <v-img :src="url_img + item.picture"></v-img>
-              </v-avatar>
-                </div>
-          </v-card>
-            </v-timeline-item>
-          </v-timeline>-->
-     
-      <v-timeline >
+     <div  >
+      <div class="d-none d-sm-inline"> 
+       <v-timeline>
         <v-timeline-item v-for="(item, i) in info" :key="i">
 
           <span slot="opposite">{{ formatDate(item.dateConcert) }}</span>
@@ -55,6 +35,42 @@
     </v-card>
         </v-timeline-item>
       </v-timeline>
+      </div>
+
+
+
+       
+      <div class="d-sm-none" v-for="(item, i) in info" :key="i">
+
+        <h3>{{ formatDate(item.dateConcert) }}</h3>
+      <v-card class="mx-auto" width="150px" style="display: table">
+
+      <v-img class="white--text align-end" height="200px" width="300px" :src="url_img + item.picture">
+        
+      </v-img>
+  
+      <v-card-title class="headline" v-text="item.chanteur"></v-card-title>
+      <v-card-subtitle v-text="item.designation"></v-card-subtitle>
+
+  
+      <v-card-actions>
+        <v-btn
+          color="orange"
+          text
+        >
+          Share
+        </v-btn>
+  
+        <v-btn
+          color="orange"
+          text
+        >
+          Explore
+        </v-btn>
+      </v-card-actions>
+    </v-card><br>
+      </div>
+      </div>
 </template>
 
 <style lang="scss">
